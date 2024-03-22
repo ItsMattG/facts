ae8ff86bf10b7910f120988f6e25e69abd83f02b
ItsMattG <gleesonmatt96@gmail.com>
2024-03-22 13:40:30 +1100

    UI setup and logic

--- resources/js/components/Facts.vue
+++ resources/js/components/Facts.vue
@@ -1,14 +1,28 @@
 <template>
-	<div class="container">
-		<h1 class="title">Choose a Fun Fact!</h1>
-		<div class="buttons">
-			<button @click="fetchFact('date')">Date</button>
-			<button @click="fetchFact('math')">Math</button>
-			<button @click="fetchFact('random')">Random</button>
-			<button @click="fetchFact('trivia')">Trivia</button>
-			<button @click="fetchFact('year')">Year</button>
+	<div class="wrapper">
+		<div class="container">
+			<h1 class="title">Choose a Fun Fact!</h1>
+			<div class="buttons">
+				<button @click="selectFact('date')" :class="{ selected: selectedFact === 'date' }">Date</button>
+				<button @click="selectFact('math')" :class="{ selected: selectedFact === 'math' }">Math</button>
+				<button @click="selectFact('random')" :class="{ selected: selectedFact === 'random' }">Random</button>
+				<button @click="selectFact('trivia')" :class="{ selected: selectedFact === 'trivia' }">Trivia</button>
+				<button @click="selectFact('year')" :class="{ selected: selectedFact === 'year' }">Year</button>
+			</div>
 		</div>
-		<div class="response" v-if="response">{{ response.text }}</div>
+		<div class="bottom-content">
+			<div class="input-field" v-if="showInputField">
+				<input v-model="inputValue" v-if="selectedFact === 'date'" type="text" placeholder="DD/MM" pattern="\d{2}/\d{2}">
+				<input v-model="inputValue" v-else type="number" placeholder="Enter a number">
+			</div>
+			<div class="fetch-button-container">
+				<button @click="fetchFact" v-if="selectedFact">Get Fact!</button>
+				<div class="response" v-if="response">{{ response.text }}</div>
+				<div v-show="showError" class="error-message">
+					Invalid date format. Please enter a valid date format (DD/MM).
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -18,27 +32,52 @@ export default {
 		return {
 			baseUrl: 'https://numbersapi.p.rapidapi.com/',
 			apiSearch: '',
-			response: {}
+			selectedFact: '',
+			inputValue: '',
+			response: {},
+			showError: false,
 		}
 	},
 	methods: {
-		async fetchFact(topic) {
-			console.log(topic);
-			switch (topic) {
+		selectFact(fact) {
+			this.showError = false;
+			if (this.selectedFact !== fact) {
+				this.selectedFact = fact;
+				this.inputValue = '';
+				this.response = '';
+			}
+		},
+		isValidDate(dateString) {
+			// Check if date is in the format DD/MM
+			const dateRegex = /^\d{2}\/\d{2}$/;
+			return dateRegex.test(dateString);
+		},
+		async fetchFact() {
+			if (!this.selectedFact) {
+				console.error('No fact selected');
+				return;
+			}
+
+			switch (this.selectedFact) {
 				case 'date':
-					this.apiSearch = '6/22/date';
+					if (!this.isValidDate(this.inputValue)) {
+						this.showError = true;
+						return;
+					}
+					this.showError = false;
+					this.apiSearch = `${this.inputValue + '/date'}`;
 					break;
 				case 'math':
-					this.apiSearch = '1729/math';
+					this.apiSearch = `${this.inputValue + '/math'}`;
 					break;
 				case 'random':
 					this.apiSearch = 'random/trivia';
 					break;
 				case 'trivia':
-					this.apiSearch = '42/trivia';
+					this.apiSearch = `${this.inputValue + '/trivia'}`;
 					break;
 				case 'year':
-					this.apiSearch = '1492/year';
+					this.apiSearch = `${this.inputValue + '/year'}`;
 					break;
 			}
 
@@ -62,6 +101,14 @@ export default {
 				console.error('Error fetching data:', error);
 			}
 		}
+	},
+	computed: {
+		showInputField() {
+			if (this.selectedFact !== '') {
+				return this.selectedFact !== 'random';
+			}
+			return false;
+		},
 	}
 }
 
